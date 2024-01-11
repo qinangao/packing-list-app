aeb41215f5bc7b572cfbf0c7662b923e2bb8776f
Qinan Gao <qinanagao@gmail.com>
2024-01-11 22:04:20 +0000

    add usestate

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
+
 function Form() {
+  const [input, setInput] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!input) return alert("input can not be emply");
+    setInput("");
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for you trip? 😍</h3>
-      <select>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (a, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
       </select>
-      <input placeholder="Item..." />
+      <input
+        placeholder="Item..."
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
 
