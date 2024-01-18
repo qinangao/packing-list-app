9b69b879ca61ca2316e76ea8acffac60509d3175
Qinan Gao <qinanagao@gmail.com>
2024-01-18 22:19:12 +0000

    lift up items state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import Form from "./components/Form";
 import Logo from "./components/Logo";
 import Packinglist from "./components/Packinglist";
 import Stats from "./components/Stats";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <Packinglist />
+      <Form onAdd={handleAddItems} />
+      <Packinglist
+        items={items}
+        onDelete={handleDeleteItems}
+        onToggle={handleToggleItems}
+      />
       <Stats />
     </div>
   );
