9b835f032c0c24393646ca66f93cae5f9826d299
Qinan Gao <qinanagao@gmail.com>
2024-01-21 21:10:18 +0000

    add calculation

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,17 @@
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list 🚀</p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <div className="stats">
-      You have x items on your list, and you alreadly packed y(z%)
+      {percentage === 100
+        ? "You got everything! Ready to go 🛬"
+        : `You have ${numItems} items on your list, and you alreadly packed
+      ${numPacked}(${percentage}%)`}
     </div>
   );
 }
